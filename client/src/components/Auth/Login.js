1857743bf71c2684579b513b778306918689fa8c
perovserg@mail.ru <perovserg@mail.ru>
2019-05-30 21:48:53 +0300

    add Context and reducer

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,46 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GraphQLClient } from 'graphql-request';
 import { GoogleLogin } from 'react-google-login';
 import { withStyles } from '@material-ui/core/styles';
-// import Typography from "@material-ui/core/Typography";
+import Typography from "@material-ui/core/Typography";
 
 import config from '../../config';
 
-const ME_QUERY = `
-{
-  me {
-    _id
-    name
-    email
-    picture
-  }
-}
-`;
+import Context from '../../context';
+import { ME_QUERY } from "../../graphql/queries";
+
 
 const Login = ({ classes }) => {
 
+  const { dispatch } = useContext(Context);
+
   const onSuccess = async googleUser => {
 
-    // receive google token id for check it on our backend
-    const idToken = googleUser.getAuthResponse().id_token;
+    try {
 
-    const clientGraphQL = new GraphQLClient(config.BACKEND_GRAPHQL_URL, {
-      headers: { authorization: idToken }
-    });
+      // receive google token id for check it on our backend
+      const idToken = googleUser.getAuthResponse().id_token;
 
-    const data = await clientGraphQL.request(ME_QUERY);
+      const clientGraphQL = new GraphQLClient(config.BACKEND_GRAPHQL_URL, {
+        headers: { authorization: idToken }
+      });
 
-    console.log('ME_QUERY response => ', data);
+      const { me } = await clientGraphQL.request(ME_QUERY);
+
+      dispatch({
+        type: 'LOGIN_USER', //todo: replace to constant
+        payload: me,
+      });
+
+    } catch (error) {
+      onFailure(error);
+    }
 
   };
 
+  const onFailure = error => {
+    console.error('Error logging in', error);
+  };
+
   return (
-      <GoogleLogin
-        onSuccess={onSuccess}
-        onFailure={() => console.error('Google login error!')}
-        clientId={config.OAUTH_GOOGLE_API_CLIENT_ID}
-        isSignedIn={true}
-      />
+      <div className={classes.root}>
+        <Typography
+            component="h1"
+            variant="h3"
+            gutterBottom
+            noWrap
+            style={{ color: "rgb(66, 133, 244)" }}
+        >
+          Welcome
+        </Typography>
+        <GoogleLogin
+            onSuccess={onSuccess}
+            onFailure={onFailure}
+            clientId={config.OAUTH_GOOGLE_API_CLIENT_ID}
+            isSignedIn={true}
+            theme="dark"
+        />
+      </div>
   );
 };
 

5a7e826813eb9e67d09c63e3de847357ab8abcd7
perovserg@mail.ru <perovserg@mail.ru>
2019-05-31 10:06:45 +0300

    add map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,32 @@
 import React from "react";
+import ReactMapGL from 'react-map-gl';
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+import config from '../config';
+
+const viewport = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13,
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>;
+  return (
+      <div className={classes.root}>
+        <ReactMapGL
+          width="100vw"
+          height="calc(100vh - 64px)"
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxApiAccessToken={config.MAPBOX_API_KEY}
+          {...viewport}
+        >
+
+        </ReactMapGL>
+      </div>
+  );
 };
 
 const styles = {
